30dbd6ca5dc63e903feb8a34448c050825360621
amer Albinqali <a0sandra0h3@gmail.com>
2023-09-02 11:10:22 -0500

    updated by co_worker

--- src/test/java/com/tutorial/qa/testcases/SearchTest.java
+++ src/test/java/com/tutorial/qa/testcases/SearchTest.java
@@ -9,6 +9,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+//update by co-worker
 public class SearchTest extends Base {
 
     public WebDriver driver;
